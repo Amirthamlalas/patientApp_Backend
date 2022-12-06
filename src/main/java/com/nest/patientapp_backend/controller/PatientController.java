6ed6ba91640219025aa41e510d5f9a4c09009d17
Amirtha M Lalas <Amirtha@NTP-LAP-746.chn.nestgroup.net>
2022-12-06 21:51:51 +0530

    added controller

--- src/main/java/com/nest/patientapp_backend/controller/PatientController.java
+++ src/main/java/com/nest/patientapp_backend/controller/PatientController.java
@@ -1,4 +1,17 @@
 package com.nest.patientapp_backend.controller;
 
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class PatientController {
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/add",consumes ="application/json",produces = "application/json")
+    public String AddPatient(){
+        return "added patient successfully";
+    }
 }
