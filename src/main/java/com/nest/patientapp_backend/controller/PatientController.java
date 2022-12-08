feb6399106575bddeae493a05b73e8148ff7aaf0
Amirtha M Lalas <Amirtha@NTP-LAP-746.chn.nestgroup.net>
2022-12-08 14:46:15 +0530

    made changes

--- src/main/java/com/nest/patientapp_backend/controller/PatientController.java
+++ src/main/java/com/nest/patientapp_backend/controller/PatientController.java
@@ -27,6 +27,26 @@ public class PatientController {
         return map;
 
     }
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/search",consumes = "application/json",produces = "application/json")
+    public List<Patients> search(@RequestBody Patients p){
+        String name=String.valueOf(p.getName());
+        System.out.println(name);
+        return (List<Patients>) dao.searchPatient(p.getName());
+    }
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/delete",consumes = "application/json",produces = "application/json")
+    public HashMap<String, String> delete(@RequestBody Patients p){
+        String id = String.valueOf(p.getId());
+        System.out.println(id);
+        dao.deletePatient(p.getId());
+        HashMap<String,String>map=new HashMap<>();
+        map.put("status","success");
+        return map;
+    }
+
     @CrossOrigin(origins = "*")
     @GetMapping("/view")
     public List<Patients>view(){
--- src/main/java/com/nest/patientapp_backend/dao/PatientDao.java
+++ src/main/java/com/nest/patientapp_backend/dao/PatientDao.java
@@ -1,7 +1,20 @@
 package com.nest.patientapp_backend.dao;
 
 import com.nest.patientapp_backend.model.Patients;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface PatientDao extends CrudRepository<Patients,Integer> {
+    @Query(value = "SELECT `id`, `address`, `dateof_appoinment`, `name`, `phoneno` FROM `patient` WHERE `name`= :name",nativeQuery = true)
+    List<Patients>searchPatient(@Param("name") String name);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM `patient` WHERE `id`= :id",nativeQuery = true)
+    void deletePatient(@Param("id")Integer id);
 }
